e8ce77fd3733d50d72960b298bc3986d1e06c039
Guillermo Biset <billy@medable.com>
2017-04-30 09:59:16 -0300

    Add sample KML file for flood risk. Use colors to draw the map.

--- Drops/Drops/Utility/KMLParser.h
+++ Drops/Drops/Utility/KMLParser.h
@@ -18,6 +18,191 @@
 @class KMLPlacemark;
 @class KMLStyle;
 
+
+@interface KMLElement : NSObject {
+    NSString *identifier;
+    NSMutableString *accum;
+}
+
+- (instancetype)initWithIdentifier:(NSString *)ident;
+
+@property (nonatomic, readonly) NSString *identifier;
+
+// Returns YES if we're currently parsing an element that has character
+// data contents that we are interested in saving.
+@property (NS_NONATOMIC_IOSONLY, readonly) BOOL canAddString;
+
+// Add character data parsed from the xml
+- (void)addString:(NSString *)str;
+
+// Once the character data for an element has been parsed, use clearString to
+// reset the character buffer to get ready to parse another element.
+- (void)clearString;
+
+@end
+
+// Represents a KML <Style> element.  <Style> elements may either be specified
+// at the top level of the KML document with identifiers or they may be
+// specified anonymously within a Geometry element.
+@interface KMLStyle : KMLElement {
+    UIColor *strokeColor;
+    CGFloat strokeWidth;
+    UIColor *fillColor;
+    
+    BOOL fill;
+    BOOL stroke;
+    
+    struct {
+        int inLineStyle:1;
+        int inPolyStyle:1;
+        
+        int inColor:1;
+        int inWidth:1;
+        int inFill:1;
+        int inOutline:1;
+    } flags;
+}
+
+- (void)beginLineStyle;
+- (void)endLineStyle;
+
+- (void)beginPolyStyle;
+- (void)endPolyStyle;
+
+- (void)beginColor;
+- (void)endColor;
+
+- (void)beginWidth;
+- (void)endWidth;
+
+- (void)beginFill;
+- (void)endFill;
+
+- (void)beginOutline;
+- (void)endOutline;
+
+- (void)applyToOverlayPathRenderer:(MKOverlayPathRenderer *)renderer;
+
+@end
+
+@interface KMLGeometry : KMLElement {
+    struct {
+        int inCoords:1;
+    } flags;
+}
+
+- (void)beginCoordinates;
+- (void)endCoordinates;
+
+// Create (if necessary) and return the corresponding Map Kit MKShape object
+// corresponding to this KML Geometry node.
+@property (NS_NONATOMIC_IOSONLY, readonly, strong) MKShape *mapkitShape;
+
+// Create (if necessary) and return the corresponding MKOverlayPathRenderer for
+// the MKShape object.
+- (MKOverlayPathRenderer *)createOverlayPathRenderer:(MKShape *)shape;
+
+@end
+
+// A KMLPoint element corresponds to an MKAnnotation and MKPinAnnotationView
+@interface KMLPoint : KMLGeometry {
+    CLLocationCoordinate2D point;
+}
+
+@property (nonatomic, readonly) CLLocationCoordinate2D point;
+
+@end
+
+@interface KMLLineString : KMLGeometry {
+    CLLocationCoordinate2D *points;
+    NSUInteger length;
+}
+
+@property (nonatomic, readonly) CLLocationCoordinate2D *points;
+@property (nonatomic, readonly) NSUInteger length;
+
+@end
+
+// A KMLPolygon element corresponds to an MKPolygon and MKPolygonView
+@interface KMLPolygon : KMLGeometry {
+    NSString *outerRing;
+    NSMutableArray *innerRings;
+    
+    struct {
+        int inOuterBoundary:1;
+        int inInnerBoundary:1;
+        int inLinearRing:1;
+    } polyFlags;
+}
+
+- (void)beginOuterBoundary;
+- (void)endOuterBoundary;
+
+- (void)beginInnerBoundary;
+- (void)endInnerBoundary;
+
+- (void)beginLinearRing;
+- (void)endLinearRing;
+
+@end
+
+@interface KMLPlacemark : KMLElement {
+    KMLStyle *style;
+    KMLGeometry *geometry;
+    
+    NSString *name;
+    NSString *placemarkDescription;
+    
+    NSString *styleUrl;
+    
+    MKShape *mkShape;
+    
+    MKAnnotationView *annotationView;
+    MKOverlayPathRenderer *overlayPathRenderer;
+    
+    struct {
+        int inName:1;
+        int inDescription:1;
+        int inStyle:1;
+        int inGeometry:1;
+        int inStyleUrl:1;
+    } flags;
+}
+
+- (void)beginName;
+- (void)endName;
+
+- (void)beginDescription;
+- (void)endDescription;
+
+- (void)beginStyleWithIdentifier:(NSString *)ident;
+- (void)endStyle;
+
+- (void)beginGeometryOfType:(NSString *)type withIdentifier:(NSString *)ident;
+- (void)endGeometry;
+
+- (void)beginStyleUrl;
+- (void)endStyleUrl;
+
+// Corresponds to the title property on MKAnnotation
+@property (nonatomic, readonly) NSString *name;
+// Corresponds to the subtitle property on MKAnnotation
+@property (nonatomic, readonly) NSString *placemarkDescription;
+
+@property (nonatomic, readonly) KMLGeometry *geometry;
+@property (unsafe_unretained, nonatomic, readonly) KMLPolygon *polygon;
+
+@property (nonatomic, strong) KMLStyle *style;
+@property (nonatomic, readonly) NSString *styleUrl;
+
+@property (NS_NONATOMIC_IOSONLY, readonly, strong) id<MKOverlay> overlay;
+@property (NS_NONATOMIC_IOSONLY, readonly, strong) id<MKAnnotation> point;
+
+@property (NS_NONATOMIC_IOSONLY, readonly, strong) MKOverlayPathRenderer *overlayPathRenderer;
+@property (NS_NONATOMIC_IOSONLY, readonly, strong) MKAnnotationView *annotationView;
+
+@end
+
 @interface KMLParser : NSObject <NSXMLParserDelegate> {
     NSMutableDictionary *_styles;
     NSMutableArray *_placemarks;
@@ -33,6 +218,7 @@
 
 @property (unsafe_unretained, nonatomic, readonly) NSArray *overlays;
 @property (unsafe_unretained, nonatomic, readonly) NSArray *points;
+@property (unsafe_unretained, nonatomic, readonly) NSArray *placemarksNonMutable;
 
 - (MKAnnotationView *)viewForAnnotation:(id <MKAnnotation>)point;
 - (MKOverlayRenderer *)rendererForOverlay:(id <MKOverlay>)overlay;
--- Drops/Drops/ViewControllers/Risks/RisksViewController.m
+++ Drops/Drops/ViewControllers/Risks/RisksViewController.m
@@ -15,38 +15,12 @@
 @property (nonatomic) NSOperationQueue *processingQueue;
 
 @property (nonatomic) IBOutlet MKMapView *map;
-@property (nonatomic) IBOutlet UIView *menuView;
-@property (nonatomic) IBOutlet UIView *menuDetailsView;
-@property (nonatomic) IBOutlet UIView *mapCoverView;
-@property (nonatomic) IBOutlet NSLayoutConstraint *menuViewHeight;
-@property (nonatomic) IBOutlet NSLayoutConstraint *menuViewWidth;
-@property (nonatomic) IBOutlet UIButton *openMenuButton;
-
-@property (nonatomic) IBOutlet UILabel *dateLabel;
-@property (nonatomic) IBOutlet UILabel *locationLabel;
-@property (nonatomic) IBOutlet UILabel *mapLabel;
-@property (nonatomic) IBOutlet UILabel *waterLabel;
-@property (nonatomic) IBOutlet UILabel *shareLabel;
-
-@property (nonatomic) IBOutlet UIButton *locationDetailsButton;
-
-@property (nonatomic) IBOutlet UIButton *dateButton;
-@property (nonatomic) IBOutlet UIButton *shareButton;
-
-@property (nonatomic) IBOutlet UISegmentedControl *mapTypeSegmentedControl;
-@property (nonatomic) IBOutlet UISegmentedControl *waterTypeSegmentedControl;
-
-@property (nonatomic) NSDateFormatter *dateFormatter;
-
-@property (nonatomic) NSDate *currentDate;
-
-@property (nonatomic) CGFloat squareHeight;
-@property (nonatomic) NSMutableSet *loadedFiles;
 
 @property (nonatomic) KMLParser *kmlParser;
 
-@property (nonatomic) NSMutableDictionary *filenamesToOverlays;
-@property (nonatomic) NSMutableDictionary *filenamesToAnnotations;
+@property (nonatomic) NSArray *placemarkNames;
+
+@property (nonatomic) NSArray *riskColors;
 
 @end
 
@@ -60,90 +34,37 @@
     
     self.map.delegate = self;
     
-    /*
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"inundacion" ofType:@"kml"];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"doc" ofType:@"kml"];
     NSURL *url = [NSURL fileURLWithPath:path];
-    [self processFile:url];
-     */
+    if ( url != nil )
+    {
+        [self processFile:url];
+    }
 }
 
+- (NSArray *)riskColors
+{
+    if ( _riskColors == nil )
+    {
+        _riskColors = @[ [UIColor colorWithHex:@"059e4d"],
+                         [UIColor colorWithHex:@"13d109"],
+                         [UIColor colorWithHex:@"5eff00"],
+                         [UIColor colorWithHex:@"cbff00"],
+                         [UIColor colorWithHex:@"eeff00"],
+                         [UIColor colorWithHex:@"ffeb00"],
+                         [UIColor colorWithHex:@"ffb200"],
+                         [UIColor colorWithHex:@"ff8a00"],
+                         [UIColor colorWithHex:@"ff0d00"],
+                         [UIColor colorWithHex:@"c40b06"]
+                         ];
+    }
+    
+    return _riskColors;
+}
 
 - (void)localizeOutlets
 {
 }
-/*
-    [self.shareButton setTitle:NSLocalizedString(@"SHARE",) forState:UIControlStateNormal];
-    [self.mapTypeSegmentedControl setTitle:NSLocalizedString(@"STANDARD", ) forSegmentAtIndex:0];
-    [self.mapTypeSegmentedControl setTitle:NSLocalizedString(@"SATELLITE", ) forSegmentAtIndex:1];
-    [self.mapTypeSegmentedControl setTitle:NSLocalizedString(@"HYBRID", ) forSegmentAtIndex:2];
-    [self.waterTypeSegmentedControl setTitle:NSLocalizedString(@"SURFACE WATER", ) forSegmentAtIndex:0];
-    [self.waterTypeSegmentedControl setTitle:NSLocalizedString(@"FLOOD WATER", ) forSegmentAtIndex:1];
-    self.dateLabel.text = NSLocalizedString(@"DATE",);
-    self.locationLabel.text = NSLocalizedString(@"LOCATION",);
-    self.mapLabel.text = NSLocalizedString(@"MAP",);
-    self.waterLabel.text = NSLocalizedString(@"WATER",);
-    self.shareLabel.text = NSLocalizedString(@"SHARE",);
-}
-
-
-- (NSDateFormatter *)dateFormatter
-{
-    if ( _dateFormatter == nil )
-    {
-        _dateFormatter = [NSDateFormatter new];
-        _dateFormatter.dateStyle = NSDateFormatterMediumStyle;
-        _dateFormatter.timeStyle = NSDateFormatterNoStyle;
-    }
-    
-    return _dateFormatter;
-}
-
-- (void)changeDate:(NSDate *)date
-{
-    [self hideMenuDetailsIfNecessary];
-    
-    if ( date == nil )
-    {
-        //Default is yesterday
-        date = [[NSDate date] dateByAddingTimeInterval:-(24 * 60 * 60 * 1)];
-    }
-    
-    if ( self.currentDate != date )
-    {
-        NSString *buttonText = [self.dateFormatter stringFromDate:date];
-        
-        [self.dateButton setTitle:buttonText forState:UIControlStateNormal];
-        
-        if ( self.currentDate != nil )
-        {
-            [self resetAnnotations];
-            self.currentDate = date;
-            [self loadAnnotations];
-        }
-        else
-        {
-            self.currentDate = date;
-        }
-    }
-}
-
-- (void)addOverlays:(NSArray *)overlays annotations:(NSArray *)annotations filename:(NSString *)filename
-{
-    [[NSOperationQueue mainQueue] addOperationWithBlock:^
-     {
-         if ( [self useMemoryCaching] )
-         {
-             self.filenamesToOverlays[filename] = overlays;
-             self.filenamesToAnnotations[filename] = annotations;
-         }
-         
-         [self.map addOverlays:overlays];
-         [self.map addAnnotations:annotations];
-         
-         [self.loadedFiles addObject:filename];
-    }];
-}
- */
 
 - (void)processFile:(NSURL *)fileURL
 {
@@ -159,7 +80,18 @@
     NSArray *annotations = [self.kmlParser points];
     
     NSString *filename = fileURL.lastPathComponent;
+    
     [self addOverlays:overlays annotations:annotations filename:filename];
+    
+    NSArray *placemarks = self.kmlParser.placemarksNonMutable;
+    
+    NSMutableArray *names = [NSMutableArray arrayWithCapacity:placemarks.count];
+    for ( KMLPlacemark *placemark in placemarks )
+    {
+        [names addObject:placemark.name ? : @"1"];
+    }
+    self.placemarkNames = [NSArray arrayWithArray:names];
+    
     [[NSOperationQueue mainQueue] addOperationWithBlock:^
      {
          [MBProgressHUD hideHUDForView:self.view animated:YES];
@@ -170,10 +102,8 @@
 {
     [[NSOperationQueue mainQueue] addOperationWithBlock:^
      {
-         [self.map addOverlays:[overlays subarrayWithRange:NSMakeRange(0, MIN(100, overlays.count - 1)) ]];
+         [self.map addOverlays:overlays];
          [self.map addAnnotations:annotations];
-         
-         [self.loadedFiles addObject:filename];
      }];
 }
 
@@ -185,21 +115,18 @@
     
     if ( [overlay isKindOfClass:[MKPolygon class]] )
     {
-        static CGFloat red = 0.1;
-        static CGFloat green = 0.7;
-        static CGFloat blue = 0.9;
+
+        NSInteger index = [self.map.overlays indexOfObject:overlay];
+        NSString *name = self.placemarkNames[index];
+        NSInteger riskNumber = [name integerValue];
+        UIColor *color = self.riskColors[riskNumber];
         
         MKPolygonRenderer *polygonRenderer = [[MKPolygonRenderer alloc] initWithPolygon:(MKPolygon *)overlay];
-        polygonRenderer.fillColor = [UIColor colorWithRed:red green:green blue:blue alpha:0.6];
-        polygonRenderer.strokeColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+        polygonRenderer.fillColor = [color colorWithAlphaComponent:0.6];
+        polygonRenderer.strokeColor = color;
         polygonRenderer.lineWidth = 2.0;
         
-        red = red + 0.07;
-        green = green - 0.02;
-        blue = blue - 0.05;
-        
         return polygonRenderer;
-
     }
 
     return nil;
@@ -212,6 +139,7 @@
 
 - (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated
 {
+    NSInteger index = 0;
     for ( MKPolygon *polygon in self.map.overlays )
     {
         MKPolygonRenderer *polygonRenderer = [[MKPolygonRenderer alloc] initWithPolygon:polygon];
@@ -222,179 +150,10 @@
         
         if ( CGPathContainsPoint(polygonRenderer.path, nil, polygonViewPoint, true) )
         {
-            NSLog(@"inside");
+            NSLog(@"Inside Risk: %@", self.placemarkNames[index]);
+            
         }
+        ++index;
     }
 }
-/*
--(void)resetAnnotations
-{
-    [self.map removeAnnotations:self.map.annotations];
-    [self.map removeOverlays:self.map.overlays];
-    [self.loadedFiles removeAllObjects];
-}
-
-- (void)loadAnnotations
-{
-    [self mapView:self.map regionDidChangeAnimated:NO];
-}
-
-#pragma mark - Action
-
-- (IBAction)toggleMenu:(id)sender
-{
-    if ( self.menuViewHeight.constant == self.squareHeight )
-    {
-        [self.openMenuButton setTitle:@"×" forState:UIControlStateNormal];
-        [self.openMenuButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
-        
-        [UIView animateWithDuration:0.25
-                         animations:^
-         {
-             self.menuView.backgroundColor = [UIColor whiteColor];
-             
-             self.menuViewWidth.constant = self.menuDetailsView.frame.size.width;
-             self.menuViewHeight.constant = self.menuDetailsView.frame.size.height;
-             
-             [UIHelper applyCornerRadius:8.0 toView:self.menuView];
-             
-             [self.view layoutIfNeeded];
-         }
-                         completion:^(BOOL finished)
-         {
-             [UIView animateWithDuration:0.15 animations:^
-              {
-                  self.menuDetailsView.alpha = 1.0;
-                  self.mapCoverView.alpha = 0.8;
-              }];
-         }];
-    }
-    else
-    {
-        [self.openMenuButton setTitle:@"+" forState:UIControlStateNormal];
-        [self.openMenuButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        
-        [UIView animateWithDuration:0.15
-                         animations:^
-         {
-             self.menuDetailsView.alpha = 0.0;
-         }
-                         completion:^(BOOL finished)
-         {
-             [UIView animateWithDuration:0.25 animations:^
-              {
-                  self.menuView.backgroundColor = [UIColor colorWithHex:@"0077cc"];
-                  
-                  self.menuViewWidth.constant = self.squareHeight;
-                  self.menuViewHeight.constant = self.squareHeight;
-                  
-                  [UIHelper applyCornerRadius:self.squareHeight / 2.0 toView:self.menuView];
-                  
-                  self.mapCoverView.alpha = 0.0;
-                  
-                  [self.view layoutIfNeeded];
-              }];
-         }];
-    }
-}
-
-- (IBAction)mapTypeChanged:(UISegmentedControl *)segmentedControl
-{
-    [self hideMenuDetailsIfNecessary];
-    
-    switch (segmentedControl.selectedSegmentIndex )
-    {
-        case 0:
-            self.map.mapType = MKMapTypeStandard;
-            break;
-        case 1:
-            self.map.mapType = MKMapTypeSatellite;
-            break;
-        default:
-            self.map.mapType = MKMapTypeHybrid;
-            break;
-    }
-}
-
-- (IBAction)waterTypeChanged:(UISegmentedControl *)segmentedControl
-{
-    [self hideMenuDetailsIfNecessary];
-    
-    switch (segmentedControl.selectedSegmentIndex )
-    {
-        case 0:
-            [FileDownloader instance].waterType = WaterTypeSurface;
-            break;
-        default:
-            [FileDownloader instance].waterType = WaterTypeFlood;
-            break;
-    }
-    
-    [self resetAnnotations];
-    [self loadAnnotations];
-}
-
-- (IBAction)tappedBackground:(id)sender
-{
-    [self hideMenuDetailsIfNecessary];
-}
-
-- (IBAction)dateSelected:(id)sender
-{
-    UIDatePicker *datePicker = [UIDatePicker new];
-    datePicker.date = self.currentDate;
-    
-    datePicker.datePickerMode = UIDatePickerModeDate;
-    
-    [datePicker addTarget:self
-                   action:@selector(dateChanged:)
-         forControlEvents:UIControlEventValueChanged];
-    
-    datePicker.backgroundColor = [UIColor principalColor];
-    [UIHelper applyCornerRadius:8.0 toView:datePicker];
-    [self.view addSubview:datePicker];
-}
-
-- (IBAction)genericShareSelected:(id)sender
-{
-    UIGraphicsBeginImageContextWithOptions(self.map.bounds.size, self.map.opaque, 0.0f);
-    [self.map drawViewHierarchyInRect:self.map.bounds afterScreenUpdates:NO];
-    UIImage *snapshotImageFromMyView = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    NSArray *items = @[snapshotImageFromMyView];
-    
-    UIActivityViewController *controller = [[UIActivityViewController alloc]initWithActivityItems:items applicationActivities:nil];
-    
-    [self.navigationDelegate.mainNavigationController presentViewController:controller
-                                                                   animated:YES
-                                                                 completion:^
-    {
-    }];
-}
-
-- (IBAction)minusOneDay:(id)sender
-{
-    [self changeDate:[self.currentDate dateByAddingTimeInterval:-24*60*60]];
-}
-
-- (IBAction)plusOneDay:(id)sender
-{
-    [self changeDate:[self.currentDate dateByAddingTimeInterval:24*60*60]];
-}
-
-- (void)dateChanged:(UIDatePicker *)datePicker
-{
-    [self changeDate:datePicker.date];
-    [datePicker removeFromSuperview];
-}
-
-- (void)hideMenuDetailsIfNecessary
-{
-    if ( self.menuViewHeight.constant != self.squareHeight )
-    {
-        [self toggleMenu:nil];
-    }
-}
-*/
 @end
